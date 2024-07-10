2022606f3e1e201a25c17ead16c99572d27811c8
uiel <parkuiery@gmail.com>
2024-07-10 11:55:06 +0900

    ⚡️ :: custom button

---
+++ react_native_game/components/PrimaryButton.js
@@ -0,0 +1,12 @@
+import { Children } from "react";
+import { View, Text } from "react-native";
+
+function PrimaryButton({ childeren }) {
+    return (
+        <View>
+            <Text>{Children}</Text>
+        </View>
+    );
+}
+
+export default PrimaryButton;
---
+++ react_native_game/screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+    return (
+        <View>
+            <TextInput />
+        </View>
+    )
+}
+
+export default StartGameScreen;
