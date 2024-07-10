d6ec6e36753d98d22301d485301d05e8ec4f0765
uiel <parkuiery@gmail.com>
2024-07-10 09:09:28 +0900

    ⚡️ :: component

--- goal_tracking_app/App.js
+++ goal_tracking_app/App.js
@@ -1,13 +1,7 @@
 import { useState } from 'react';
-import { 
-  StyleSheet,
-  Text, 
-  TextInput,
-  View,
-  Button,
-  ScrollView,
-  FlatList,
-} from 'react-native';
+import { StyleSheet, TextInput, View, Button, FlatList } from 'react-native';
+
+import GoalItem from "./components/GoalItem"
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -41,11 +35,7 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem/>;
           }}
           keyExtractor={(item, index) => {
             return item.id;
@@ -81,14 +71,5 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc"
-  },
-  goalText: {
-    color: "white",
   },
 });
