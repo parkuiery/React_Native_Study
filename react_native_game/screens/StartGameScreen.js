5efdad87ffd9ca22a696715550ad5ab905ac2177
uiel <parkuiery@gmail.com>
2024-07-10 11:55:52 +0900

    ⚡️ :: primaryButton

--- react_native_game/screens/StartGameScreen.js
+++ react_native_game/screens/StartGameScreen.js
@@ -5,6 +5,8 @@ function StartGameScreen() {
     return (
         <View>
             <TextInput />
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
         </View>
     )
 }
